869c0ae0724b2a683b8ba584c1166cda4f598dcf
Roman Krinitsyn <rkrin@mail.ru>
2019-11-16 09:06:03 +0300

    add help command

--- src/main/java/com/krinroman/vkbot/string/ChangeString.java
+++ src/main/java/com/krinroman/vkbot/string/ChangeString.java
@@ -2,11 +2,6 @@ package com.krinroman.vkbot.string;
 
 public class ChangeString {
     public static String RemoveSign(String str){
-        String signStr = ".,/?+-_*\\|:;%$#@!\"\'<>{}[]()";
-
-        for(int i = 0; i < signStr.length();i++){
-            str = str.replace(signStr.charAt(i),'\0');
-        }
-
+        return str.replace("?","");
     }
 }
